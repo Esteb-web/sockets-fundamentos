bdee2aca5402920a0ce3e0e1fca21bafc3aaf4f7
Esteban <encuentroacademia@gmail.com>
2020-08-17 12:59:23 -0700

    Primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,22 @@
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+// escuchar
+socket.on('disconnect', function() {
+    console.log('Perdimos conexion con el servidor');
+});
+
+//Enviar Informacion
+socket.emit('enviarMensaje', {
+    usuario: 'Esteban',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('respuesta server: ', resp);
+});
+
+// Escuchar informacion
+socket.on('enviarMensaje', function(mensaje) {
+    console.log('Servidor:', mensaje);
+});
